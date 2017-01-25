13570eb529f017a57c964451d2e63a29f3a21560
Elliot Davies <elliot.davies@the-times.co.uk>
2017-01-25 16:32:16 +0000

    Progress

--- handler.js
+++ handler.js
@@ -1,6 +1,12 @@
 'use strict';
 
+const fetch = require('node-fetch');
+
 const timesToken = 'vhdAS4B9roqEjYS08Is1thmb';
+
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const redirectUri = process.env.REDIRECT_URI;
 
 module.exports.handler = (event, context, callback) => {
 
@@ -28,23 +34,43 @@ const sendResponse = callback => data => {
 
 
 const getHandler = (respond, event) => {
-  respond({
-    message: 'Function executed successfully!',
-    input: event,
-  });
+
+  let code;
+  try {
+    code = event.queryStringParameters.code;
+  } catch (e) {
+    respond({ error: `Couldn't retrieve code from ${event.queryStringParameters}`, event })
+    return;
+  }
+
+  const url = `https://slack.com/api/oauth.access?client_id=${clientId}&client_secret=${clientSecret}&code=${code}&redirect_uri=${redirectUri}`
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log('OAuth completed successfully');
+      respond({
+        data
+      });
+    });
 }
 
 
 const postHandler = (respond, event) => {
+
+  console.log(event.body);
+
   let postBody;
   try {
     postBody = JSON.parse(event.body);
   } catch (e) {
-    respond({ error: e });
+    console.log(`Error parsing event body`);
+    respond({ error: `Error parsing event body: ${event.body}` });
     return;
   }
 
   if (postBody.token !== timesToken) {
+    console.log(`Error: Token didn't match`);
     respond({ error: `Error: Token didn't match` });
     return;
   }
@@ -54,6 +80,7 @@ const postHandler = (respond, event) => {
       respond({ challenge: postBody.challenge });
       return;
     default:
+      console.log(`Received type ${postBody.type}`);
       respond({ error: `Unrecognised type: ${postBody.type}` });
       return;
   };
