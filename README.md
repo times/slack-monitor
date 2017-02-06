4e2f34bb980c96ff9df82eade9e134ee69d852d5
Elliot Davies <elliot.davies@the-times.co.uk>
2017-02-06 15:53:49 +0000

    Tidy up repo

--- README.md
+++ README.md
@@ -7,9 +7,11 @@ Slack Monitor is built with [Serverless](http://serverless.com/framework/docs), 
 
 ### Structure
 
-The main logic is contained in `handler.js`, which must expose a `handler` function for Serverless to call.
+The main logic is contained in `src/handler.js`, which must expose a `handler` function for Serverless to call.
 
-Since this app can be installed by multiple teams, a DynamoDB table is used for storage. Slack Monitor currently expects this table to already exist.
+Since this app can be installed by multiple teams, a DynamoDB table is used for storage. Slack Monitor currently expects this table to already exist and to be called `slackmonitor`.
+
+The mapping from Slack events to nicely-formatted notifications is handled in `events.js`.
 
 The app is installed via an "Add to Slack" button. `index.html` provides a minimal HTML page that hosts such a button with the correct scopes and parameters.
 
@@ -21,9 +23,12 @@ Serverless configuration is handled via `serverless.yml`.
     npm install
 
 
+AWS Lambda currently supports NodeJS 4.3. If you want to use features from later versions of Node you will need to use Babel or a similar tool.
+
+
 ### Deployment
 
-To deploy the main app to Lambda:
+To deploy the main app to Lambda, check the `serverless.yml` configuration and then run:
 
     serverless deploy
 
@@ -33,6 +38,11 @@ To deploy the `index.html` file containing the "Add to Slack" button, simply cop
 ### Install the app for your Slack team
 
 Navigate to the index.html page. Click the "Add to Slack button" and follow the OAuth process.
+
+
+### Contributing
+
+Please log issues on the GitHub repository. Contributions are welcome via pull requests and should preferably relate to an existing issue.
 
 
 ### Contact
--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,10 @@
 'use strict';
 
+// Libraries
 const fetch = require('node-fetch');
 const aws = require('aws-sdk');
+
+const eventMappings = require('./events');
 
 // env parameters
 const clientId = process.env.CLIENT_ID;
@@ -10,6 +13,7 @@ const redirectUri = process.env.REDIRECT_URI;
 
 // Instantiate DB connection
 const dynamoDB = new aws.DynamoDB();
+
 
 /**
  * Serverless entry point
@@ -25,13 +29,12 @@ module.exports.handler = (event, context, callback) => {
     case 'GET':
       oAuthHandler(respond, event);
       return;
-    // Most requests from Slack will be via POST
+    // Requests from Slack will be via POST
     case 'POST':
       postHandler(respond, event);
       return;
   };
 };
-
 
 
 /**
@@ -44,7 +47,7 @@ const getTeamFromDB = (teamId, cb) => {
        S: teamId
       }, 
     },
-    TableName: "lighthouse-bot"
+    TableName: "slackmonitor"
   };
 
   dynamoDB.getItem(dynamoParams, (err, data) => {
@@ -77,7 +80,7 @@ const storeTeamInDB = (teamId, teamName, accessToken, webhookUrl, cb) => {
         S: webhookUrl
       }
     },
-    TableName: "lighthouse-bot"
+    TableName: "slackmonitor"
   };
 
   dynamoDB.putItem(dynamoParams, (err, data) => {
@@ -131,7 +134,6 @@ const oAuthHandler = (respond, event) => {
       } else {
         console.log('OAuth completed successfully');
         storeTeamInDB(data.team_id, data.team_name, data.access_token, data.incoming_webhook.url, (res) => {
-          console.log(res);
           respond('OAuth completed successfully');
         });
       }
@@ -182,8 +184,6 @@ const postHandler = (respond, event) => {
  */
 const handleEvent = (respond, webhookUrl, event) => {
 
-  console.log(event);
-
   const eventDetails = eventMappings[event.type](event);
   const response = {
     attachments: [
@@ -195,7 +195,7 @@ const handleEvent = (respond, webhookUrl, event) => {
     ]
   };
   
-  // Just use respond here?
+  // TODO: Just use respond here?
   fetch(webhookUrl, {
     method: 'POST',
     headers: {
@@ -207,142 +207,4 @@ const handleEvent = (respond, webhookUrl, event) => {
   .catch(err => console.log('Slack webhook returned an error:', err));
 
   respond();
-};
-
-
-/**
- * Map event types to proper names and descriptions
- */
-const eventMappings = {
-
-  // Team settings
-  team_rename: event => ({
-    name: 'Team renamed',
-    desc: `The team was renamed to ${event.name}`,
-  }),
-
-  team_domain_change: event => ({
-    name: 'Team domain changed',
-    desc: `The team domain was changed to ${event.domain} so the URL is now ${event.url}`,
-  }),
-
-  email_domain_changed: event => ({
-    name: 'Team email domain changed',
-    desc: `The team email domain was changed to ${event.email_domain}`,
-  }),
-
-
-  // Channels
-  channel_created: event => ({
-    name: 'Channel created',
-    desc: `<#${event.channel.id}|${event.channel.name}> was created by <@${event.channel.creator}>`,
-  }),
-
-  channel_deleted: event => ({
-    name: 'Channel deleted',
-    desc: `<#${event.channel}> was deleted`,
-  }),
-
-  channel_rename: event => ({
-    name: 'Channel renamed',
-    desc: `<#${event.channel.id}|${event.channel.name}> was renamed`,
-  }),
-
-  channel_archive: event => ({
-    name: 'Channel archived',
-    desc: `<#${event.channel}> was archived by <@${event.user}>`,
-  }),
-
-  channel_unarchive: event => ({
-    name: 'Channel resurrected',
-    desc: `<#${event.channel}> was un-archived by <@${event.user}>`,
-  }),
-
-
-  // Users
-  team_join: event => {
-
-    let userType;
-    if (event.user.is_bot) userType = 'bot';
-    else if (event.user.is_restricted) userType = 'guest';
-    else if (event.user.is_ultra_restricted) userType = 'single channel guest';
-    else userType = 'regular user';
-
-    return {
-      name: 'New user created',
-      desc: `<@${event.user.id}> joined the team as a ${userType}`,
-    };
-  },
-
-  user_change: event => ({
-    name: 'User details updated',
-    /**
-     * To do:
-     *  – Is the user now disabled?
-     *  – Does the user have a profile picture?
-     */
-    desc: `<@${event.user.id}>’s details were updated`,
-  }),
-
-
-  // User groups
-  subteam_created: event => ({
-    name: 'New user group created',
-    desc: `The user group <!subteam^${event.subteam.id}|${event.subteam.name}> with the description "${event.subteam.description}" was created by <@${event.subteam.created_by}>`,
-  }),
-
-  subteam_updated: event => ({
-    name: 'User group updated',
-    desc: `The user group <!subteam^${event.subteam.id}|${event.subteam.name}> was updated by <@${event.subteam.updated_by}>`,
-  }),
-
-
-  // Files
-  file_comment_added: event => ({
-    name: 'File comment added',
-    desc: `A file comment was added by <@${event.comment.user}>`,
-  }),
-
-  file_comment_edited: event => ({
-    name: 'File comment edited',
-    desc: `A file comment was edited by <@${event.comment.user}>`,
-  }),
-
-  file_public: event => ({
-    name: 'File made public',
-    // This doesn't work
-    desc: `The file <${event.file.permalink}|${event.file.title}> was made public`,
-  }),
-
-
-  // Emoji
-  emoji_changed: event => {
-
-    let name, desc;
-    switch (event.subtype) {
-      case 'add':
-        name = 'Custom emoji added';
-        desc = `A new custom emoji was created: :${event.name}:`
-        break;
-      case 'remove':
-        if (event.names.length === 1) {
-          name = 'Custom emoji removed';
-          desc = `The custom emoji :${event.names[0]}: was removed`;
-        } else {
-          name = 'Custom emojis removed';
-          desc = `The following custom emojis were removed: ${event.names[0].map(n => `:${n}:`).join(' ')}`;
-        }
-        break;
-      default:
-        name = 'Custom emoji event';
-        desc = `Something happened with a custom emoji, but I'm not sure what. The event type was ${event.subtype}`
-        break;        
-    }
-
-    return {
-      name,
-      desc,
-    }
-  },
-
 };
